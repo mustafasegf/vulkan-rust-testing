0befb6be1fddbbbba2249d9269c0f4f680d6aa54
r17x <ri7nz@evilfactory.id>
2024-11-24 00:59:29 +0700

    chore: use rustPlatform and reconfigure devShell

--- flake.nix
+++ flake.nix
@@ -2,49 +2,109 @@
   description = "A devShell example";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        overlays = [ (import rust-overlay) ];
+        overlays = [
+          rust-overlay.overlays.default
+        ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-      in
-      with pkgs;
-      {
-        devShells.default = mkShell {
-
-          nativeBuildInputs = [ ];
-          buildInputs = [
-            cmake
-            pkg-config
-            libtool
-            fontconfig
-
-            vulkan-loader.out
-            xorg.libX11.out
-            xorg.libXcursor.out
-            xorg.libXrandr.out
-            xorg.libXi.out
-
-            rust-bin.stable.latest.complete
-
-          ];
-
-          shellHook = ''
-            LD_LIBRARY_PATH=$(nix eval --raw nixpkgs#vulkan-loader.out)/lib:$LD_LIBRARY_PATH
-            LD_LIBRARY_PATH=$(nix eval --raw nixpkgs#xorg.libX11.out)/lib:$LD_LIBRARY_PATH
-            LD_LIBRARY_PATH=$(nix eval --raw nixpkgs#xorg.libXcursor.out)/lib:$LD_LIBRARY_PATH
-            LD_LIBRARY_PATH=$(nix eval --raw nixpkgs#xorg.libXrandr.out)/lib:$LD_LIBRARY_PATH
-            LD_LIBRARY_PATH=$(nix eval --raw nixpkgs#xorg.libXi.out)/lib:$LD_LIBRARY_PATH
-          '';
-
+        optional = {
+          darwin = pkgs.lib.optionals pkgs.stdenv.isDarwin;
+          linux = pkgs.lib.optionals pkgs.stdenv.isLinux;
         };
+        defaultPackage =
+          with pkgs;
+          rustPlatform.buildRustPackage {
+            pname = "enabler";
+            version = "0.0.0";
+            cargoLock.lockFile = ./Cargo.lock;
+            src = lib.cleanSource ./.;
+
+            # LIBTOOL = "${lib.getExe glibtool}";
+            ## Compiler or Toolchain flags
+            # CXXFLAGS = "-Wno-unused-variable -Wno-unused-parameter -Wunused-parameter";
+            # CFLAGS = "-Wno-unused-variable -Wunused-parameter";
+            # cmakeFlags = [
+            #   "-Wno-dev"
+            #   "-DCMAKE_BUILD_TYPE=Release"
+            # ];
+            cmakeFlags = optional.darwin [
+              "-DCMAKE_AR=${lib.getExe glibtool}"
+            ];
+
+            nativeBuildInputs =
+              [
+                shaderc
+                clang
+                cmake
+                git
+                python3
+                glibtool
+                pkg-config
+                vulkan-loader
+                vulkan-tools
+                vulkan-headers
+              ]
+              ++ optional.darwin [
+                glibtool
+                automake
+                autoconf
+              ];
+
+            buildInputs =
+              [
+                gcc
+                cmake
+                shaderc
+                vulkan-loader
+              ]
+              ++ optional.darwin [
+                glibtool
+                automake
+                autoconf
+                darwin.moltenvk
+              ];
+
+            LD_LIBRARY_PATH = lib.makeLibraryPath (
+              [
+                shaderc
+                vulkan-loader
+              ]
+              ++ optional.linux [
+                xorg.libX11
+                xorg.libXcursor
+                xorg.libXrandr
+                xorg.libXi
+              ]
+            );
+          };
+      in
+      {
+        packages.default = defaultPackage;
+        devShells.default =
+          with pkgs;
+          mkShell {
+            inputsFrom = [ defaultPackage ];
+            shellHook = optional.darwin ''
+              export DYLD_LIBRARY_PATH=${vulkan-loader}/lib:$DYLD_LIBRARY_PATH
+            '';
+          };
       }
     );
 }
